7190bb4994cfe6950be42c02e878edcadeda90a2
dineshBS008 <dineshsaud008@gmail.com>
2024-05-18 22:43:50 +0545

    jQuery problem resolved

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React  from 'react';
 import {Routes,Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Policy from './pages/Policy';
+import Pagenotfound from './pages/Pagenotfound';
 
 function App() {
   return (
     <>
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/policy" element={<Policy />} />
+      <Route path="*" element={<Pagenotfound />} />
+      {/* * means if above pages is not found then run this last one */}
     </Routes>
     
     </>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header components</h1>
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <NavLink to="/" className="navbar-brand" href="#">Hidden brand</NavLink>
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link">Home</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/category" className="nav-link">Category</NavLink>
+        </li>
+
+        <li className="nav-item">
+          <NavLink to="/register" className="nav-link" href="#">Register</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/login" className="nav-link" href="#">Login</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/cart" className="nav-link" href="#">Cart(0)</NavLink>
+        </li>
+       
+      </ul>
+      
     </div>
+  </div>
+</nav>
+
+    </>
   );
 };
 
-export default Header
+export default Header;
