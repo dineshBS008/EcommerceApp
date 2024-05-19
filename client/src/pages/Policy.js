816ba07f9db234feb88a55a4bc86cf78e5ad93e9
dineshBS008 <dineshsaud008@gmail.com>
2024-05-20 01:26:39 +0545

    image components added

--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import {BiMailSend, BiPhoneCall, BiSupport} from "react-icons/bi";
 
 const Policy = () => {
   return (
     <Layout>
-        <h1>Privacy Policy</h1>
+        <div className='row contactus'>
+          <div className='col-md-6'> 
+            <img
+              src='/images/contactus.jpeg' 
+              alt='contactus'
+              style={{width: "100%" }}
+            />
+          </div>
+          <div className='col-md-4'>
+            
+            <p className='text-justify mt-2'>
+              add privacy policy
+            </p>
+            <p className='mt-3'>
+              add privacy policy
+            </p>
+            <p className='mt-3'>
+              add privacy policy
+            </p>
+            <p className='mt-3'>
+              add privacy policy
+            </p>
+          </div>
+        </div>
     </Layout>
   );
 };
