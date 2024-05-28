e39d15e77ba50da9f2d4fe0d6c24efa0d4a2f8b1
dineshBS008 <dineshsaud008@gmail.com>
2024-05-29 02:42:08 +0545

    front end and backend merged with a code

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
+import cors from "cors";
 
 //configure env
 dotenv.config();
@@ -17,6 +18,7 @@ const app = express();
 
 
 //middelwares
+app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
