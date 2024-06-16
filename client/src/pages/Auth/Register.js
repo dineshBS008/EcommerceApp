88ba4165f1b743b3f7f2afb5eeef51c0f1c45555
dineshBS008 <dineshsaud008@gmail.com>
2024-06-16 23:11:49 +0545

    checkPoint

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,66 +1,110 @@
-import React,{useState} from 'react';
-import axios from 'axios';
-import {useNavigate} from 'react-router-dom';
-import Layout from '../../components/Layout';
-import {toast} from 'react-toastify';
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Layout from "../../components/Layout";
+import { toast } from "react-toastify";
 
 const Register = () => {
-    const [name,setName] = useState("")
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
-    const [phone,setPhone] = useState("")
-    const [address,setAddress] = useState("")
-    const navigate = useNavigate()
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const navigate = useNavigate();
 
-    // form function
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
-          const res = await axios.post('/api/v1/auth/register', {name, email, password, phone, address});
-          if(res.data.success){
-            toast.success(res.data.message);
-            navigate("/login");
-          } else {
-            toast.error(res.data.message);
-          }
-      } catch (error) {
-          console.log(error);
-          toast.error('Something went wrong');
+  // form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/v1/auth/register", {
+        name,
+        email,
+        password,
+        phone,
+        address,
+      });
+      if (res.data.success) {
+        toast.success(res.data.message);
+        navigate("/login");
+      } else {
+        toast.error(res.data.message);
       }
-    };
-   
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
   return (
-    <Layout title = "Register - Ecommerce App" >
-      <div className='register'>
+    <Layout title="Register - Ecommerce App">
+      <div className="register">
         <h1>Register Page</h1>
-       
-       <form onSubmit={handleSubmit}>
 
-  <div className="mb-3">
-    
-    <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="form-control" id="exampleInputEmail1" placeholder='Enter Your Name' required /> 
-  </div>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Name"
+              required
+            />
+          </div>
 
-  <div className="mb-3">
-    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" id="exampleInputEmail1" placeholder='Enter Your Email' required  /> 
-  </div>
+          <div className="mb-3">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Email"
+              required
+            />
+          </div>
 
-  <div className="mb-3">
-   
-    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" id="exampleInputPassword1" placeholder='Enter Your Password' required  />
-  </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter Your Password"
+              required
+            />
+          </div>
 
-  <div className="mb-3">
-    <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} className="form-control" id="exampleInputEmail1" placeholder='Enter Your Phone' required /> 
-  </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Phone"
+              required
+            />
+          </div>
 
-  <div className="mb-3">
-    <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} className="form-control" id="exampleInputEmail1" placeholder='Enter Your Address' required /> 
-  </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Address"
+              required
+            />
+          </div>
 
-  <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
-
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
       </div>
     </Layout>
   );
--- controllers/authController.js
+++ controllers/authController.js
@@ -2,116 +2,114 @@ import userModel from "../models/userModel.js";
 import { comparePassword, hashPassword } from "../helpers/authHelper.js";
 import JWT from "jsonwebtoken";
 
-export const registerController = async(req, res) => {
-    try {
-        const {name,email,password,phone,address} = req.body
-        //validations
-        if(!name){
-            return res.send({message:'Name is Required'});
-        }
-        if(!email){
-            return res.send({message:'Email is Required'});
-        }
-        if(!password){
-            return res.send({message:'Password is Required'});
-        }
-        if(!phone){
-            return res.send({message:'Phone no is Required'});
-        }
-        if(!address){
-            return res.send({message:'Address is Required'});
-        }
-
-       //check user
-        const existingUser = await userModel.findOne({ email });
-        //existing user
-        if (existingUser) {
-            return res.status(200).send({
-                success: false,
-                message:'Already Registered please login',
-            });
-        }
-        //register user
-        const hashedPassword = await hashPassword(password)
-        //save 
-        const user = await new userModel({
-            name,
-            email,
-            phone,
-            address,
-            password:hashedPassword,
-        }).save();
-
-        res.status(201).send({
-            success: true,
-            message: 'User Registered Successfully',
-            user
-        });
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-            success: false,
-            message: 'Error in Registration',
-            error
-        })
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
+    //validations
+    if (!name) {
+      return res.send({ message: "Name is Required" });
     }
-};
+    if (!email) {
+      return res.send({ message: "Email is Required" });
+    }
+    if (!password) {
+      return res.send({ message: "Password is Required" });
+    }
+    if (!phone) {
+      return res.send({ message: "Phone no is Required" });
+    }
+    if (!address) {
+      return res.send({ message: "Address is Required" });
+    }
 
+    //check user
+    const existingUser = await userModel.findOne({ email });
+    //existing user
+    if (existingUser) {
+      return res.status(200).send({
+        success: false,
+        message: "Already Registered please login",
+      });
+    }
+    //register user
+    const hashedPassword = await hashPassword(password);
+    //save
+    const user = await new userModel({
+      name,
+      email,
+      phone,
+      address,
+      password: hashedPassword,
+    }).save();
+
+    res.status(201).send({
+      success: true,
+      message: "User Registered Successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Registration",
+      error,
+    });
+  }
+};
 
 //METHOD = POST LOGIN
 export const loginController = async (req, res) => {
-    try {
-        const {email, password} = req.body
-        //validation
-        if (!email || !password) {
-            return res.status(404).send({
-                success: false,
-                message: 'Invalid email or password',
-            });
-        }
-        //check user
-        const user = await userModel.findOne({email})
-        if (!user) {
-            return res.status(404).send({
-                success: false,
-                message: 'Email is not registerd',
-            });
-        }
-        //comparing
-        const match = await comparePassword(password, user.password);
-        if(!match){
-            return res.status(200).send({
-                success: false,
-                message: 'Invalid Password',
-            });
-        }
-        //token
-        const token = await JWT.sign({_id:user._id}, process.env.JWT_SECRET, {
-            expiresIn: '7d',
-        });
-        res.status(200).send({
-            success: true,
-            message: 'login successfully',
-            user:{
-                name: user.name,
-                email: user.email,
-                phone: user.phone,
-                address: user.address
-            },
-            token,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-            success: false,
-            message: 'Error in login',
-            error,
-        });
+  try {
+    const { email, password } = req.body;
+    //validation
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid email or password",
+      });
     }
+    //check user
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Email is not registerd",
+      });
+    }
+    //comparing
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid Password",
+      });
+    }
+    //token
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(200).send({
+      success: true,
+      message: "login successfully",
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in login",
+      error,
+    });
+  }
 };
 
 //test controller
 export const testController = (req, res) => {
-    res.send("Protected Route");
+  res.send("Protected Route");
 };
