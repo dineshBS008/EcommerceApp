9393e549f5f3dbf0331d81fa6bcd0369d6f44002
dineshBS008 <dineshsaud008@gmail.com>
2024-09-02 01:43:21 +0545

    deployment changes updated

--- server.js
+++ server.js
@@ -8,12 +8,18 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
 
 //database config
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
